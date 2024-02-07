3bd58c274a14be594beb788c1a458c18ea62c9be
celestebatres <celesbatres@gmail.com>
2024-02-07 00:32:06 +0000

    Descomprimiendo lab2

---
+++ labs/lab2/Bootloader/Files/boot/main.c
@@ -0,0 +1,44 @@
+
+/**********************************************************************
+ * This a dirt simple boot loader, whose sole job is to boot
+ * an ELF kernel image from the first IDE hard disk.
+ *
+ * DISK LAYOUT
+ *  * This program(boot.S and main.c) is the bootloader.  It should
+ *    be stored in the first sector of the disk.
+ * 
+ *  * The 2nd sector onward holds the kernel image.
+ *	
+ *  * The kernel image must be in ELF format.
+ *
+ * BOOT UP STEPS	
+ *  * when the CPU boots it loads the BIOS into memory and executes it
+ *
+ *  * the BIOS intializes devices, sets of the interrupt routines, and
+ *    reads the first sector of the boot device(e.g., hard-drive) 
+ *    into memory and jumps to it.
+ *
+ *  * Assuming this boot loader is stored in the first sector of the
+ *    hard-drive, this code takes over...
+ *
+ *  * control starts in bootloader.S -- which sets up protected mode,
+ *    and a stack so C code then run, then calls bootmain()
+ *
+ *  * bootmain() in this file takes over, reads in the kernel and jumps to it.
+ **********************************************************************/
+
+
+
+void
+bootmain(void)
+{
+	//This is where the fun begins!!
+	//To be continued...
+	int i;
+	while (1)
+	{/* do nothing */
+	
+	}
+}
+
+
